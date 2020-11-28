bed5539fc1708d43035c8b1bb259a7da6b77b685
Kory Nelson <kory.nelson99@gmail.com>
2020-11-28 11:22:58 -0800

    added basic table component and random data

---
+++ src/components/table/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const DataTable = ({data}) => {
+
+    return(
+        <div>
+            <table>
+                <thead>
+                    table head here
+                </thead>
+                <tbody>
+                    table body here
+                </tbody>
+            </table>
+        </div>
+    )
+};
+
+export default DataTable;
